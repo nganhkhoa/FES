6266eac675551364c6119eae5cfe8e7047bf6df6
Nguyễn Anh Khoa <ng.akhoa@yahoo.com.vn>
2019-03-21 21:51:46 +0700

    modify: rpc reducer/saga call

--- src/reducers/rpc.js
+++ src/reducers/rpc.js
@@ -1,37 +1,37 @@
-const addFolder = (state, payload) => {
+const addFile = (state, payload) => {
   const { file } = payload;
-  const index = state.folderList.indexOf(file);
+  const index = state.fileList.indexOf(file);
   if (index !== -1) return state;
 
-  console.log("Add file", file);
+  console.log('Add file', file);
   return Object.assign({}, state, {
     ...state,
-    folderList: [...state.folderList, file]
+    fileList: [...state.fileList, file]
   });
 };
 
-const removeFolder = (state, payload) => {
+const removeFile = (state, payload) => {
   const { file } = payload;
-  console.log("Remove file", file);
-  const index = state.folderList.indexOf(file);
-  const newFileList = state.folderList.slice();
+  console.log('Remove file', file);
+  const index = state.fileList.indexOf(file);
+  const newFileList = state.fileList.slice();
   newFileList.splice(index, 1);
   return {
     ...state,
-    folderList: newFileList
+    fileList: newFileList
   };
 };
 
 const rpcReducer = (state = {}, { type, payload }) => {
   switch (type) {
-    case "rpc/addFolder":
-      return addFolder(state, payload);
-    case "rpc/removeFolder":
-      return removeFolder(state, payload);
-    case "rpc/clearFolder":
+    case 'rpc/addFile':
+      return addFile(state, payload);
+    case 'rpc/removeFile':
+      return removeFile(state, payload);
+    case 'rpc/clearFile':
       return Object.assign({}, state, {
         ...state,
-        folderList: []
+        fileList: []
       });
     default:
       return state;
--- src/sagas/rpc.js
+++ src/sagas/rpc.js
@@ -1,28 +1,60 @@
-import { put, takeLatest, select } from "redux-saga/effects";
+import { put, takeLatest, call, fork, select, all } from 'redux-saga/effects';
 
-function* encryptFolder({ payload }) {
+const rpc_callback = (err, res, more) => {
+  if (!more) {
+    console.log(res);
+    console.log('done');
+  } else {
+    console.log(res);
+  }
+};
+
+function* encryptFile({ payload }) {
   const { rpc } = yield select();
-  const { zerorpc, folderList } = rpc;
+  const { zerorpc, fileList } = rpc;
   const { algo } = payload;
-  console.log("RPC call encrypt_folder");
-  // TODO: Make await
-  folderList.forEach(folder => {
-    console.log("CALL: ", folder);
-    zerorpc.invoke(
-      "encrypt_folder",
-      algo,
-      folder.path,
-      "keyfile.PEM",
-      (err, res, more) => {
-        console.log(res);
-      }
-    );
-  });
-  yield put({ type: "rpc/clearFolder" });
+
+  console.log('RPC call encrypt');
+  yield all(
+    fileList.map(file => {
+      console.log('CALL: ', file);
+      return fork(
+        zerorpc.invoke,
+        'encrypt',
+        algo,
+        file.path,
+        'keyfile.PEM',
+        rpc_callback
+      );
+    })
+  );
+  yield put({ type: 'rpc/clearFile' });
+}
+
+function* decryptFile({ payload }) {
+  const { rpc } = yield select();
+  const { zerorpc, fileList } = rpc;
+  const { algo } = payload;
+  console.log('RPC call decrypt');
+  yield all(
+    fileList.map(file => {
+      console.log('CALL: ', file);
+      return fork(
+        zerorpc.invoke,
+        'decrypt',
+        algo,
+        file.path,
+        'keyfile.PEM',
+        rpc_callback
+      );
+    })
+  );
+  yield put({ type: 'rpc/clearFile' });
 }
 
 function* actionWatcher() {
-  yield takeLatest("rpc/encryptFolder", encryptFolder);
+  yield takeLatest('rpc/encrypt', encryptFile);
+  yield takeLatest('rpc/decrypt', decryptFile);
 }
 
 export default actionWatcher;
